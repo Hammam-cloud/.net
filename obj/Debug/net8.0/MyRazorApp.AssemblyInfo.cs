de3037d2d29b2a7d60253d211d83fd74e2f06282
m.kassem <m.kassem@3ds-datacom.com>
2025-12-16 19:24:00 +0200

    Add databas config

--- obj/Debug/net8.0/MyRazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a817d9a4c0a960373ceed718d7bbb01087a5b46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72dbbdd224c9ca536ed33a4dc00ee0d6c5544a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
